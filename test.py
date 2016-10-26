bedb0a906911f0bb7cda3595aee4331a60b9b3a4
DiXuDelft <d.xu-1@tudelft.nl>
2016-10-26 09:13:49 +0000

    my first commit

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print('hello world')
-print('hello world')
+import numpy
+import numpy as np
+def circumference(x):
+    y = 2*np.pi*x
+    print(y)
+
+def area(x):
+    y = np.pi*x**2
+    print(y)
